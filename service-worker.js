708251ade6287c990229cdab0824eeb04617fb23
jheilo87 <jheilo1006@gmail.com>
2026-07-29 21:29:09 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='raysmart-v1.0-20260729';
+const ASSETS=['./','./index.html','./search.html','./sn.html','./calendar.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./material.xlsx','./Calendar.xlsx'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
